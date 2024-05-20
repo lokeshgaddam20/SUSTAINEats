9a1b82b3155f20fb47242cad80796b85f182205f
srimanth <srimanthreddynukala@gmail.com>
2024-05-20 14:54:29 +0530

    food controller update

---
+++ backend/controller/food.controller.js
@@ -0,0 +1,12 @@
+async function getAllFoods(req,res){
+    res.send('getAllFoods')
+}
+async function getFood(req,res){
+    res.send('getFood')
+}
+async function getFoodbyID(req,res){
+    res.send('getFoodbyID')
+}
+async function getSeasonalFood(req,res){
+    res.send('getSeasonalFood')
+}
