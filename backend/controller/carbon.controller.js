f418a270d8cdb9e8e6972303c81fcc9a5ec9a95d
puntikuraakhilesh <akhilesh.p1211@gmail.com>
2024-05-24 14:40:06 +0530

    Update:Carbon Controller Semi

--- backend/controller/carbon.controller.js
+++ backend/controller/carbon.controller.js
@@ -1,6 +1,18 @@
-async function calculateCarbonFootPrint(req,res){
-    res.status(200).send("calculateCarbonFootPrint")
+const MealPlan = require('../models/meal.model');
+
+async function calculateCarbonFootPrint(req, res) {
+    try {
+       const find=req.params.find
+        const data = await MealPlan.findOne({ user: req.user, name: find },'recipes');
+        
+        if (!data) {
+            return res.status(404).json({ message: 'Meal plan not found' });
+        }
+
+        res.status(200).json(data.recipes);
+    } catch (error) {
+        res.status(500).json({ message: 'Server error', error: error.message });
+    }
 }
 
-
-module.exports={calculateCarbonFootPrint}
+module.exports = { calculateCarbonFootPrint };
